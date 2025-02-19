f6696551483d1d6e2f178fa90f0497020ca2eaec
MaKaNu <matti.kaupenjohann@fh-dortmund.de>
2025-02-19 09:58:43 +0100

    Begin fixing typeerrors

--- pytorchimagepipeline/pipelines/sam2segnet/errors.py
+++ pytorchimagepipeline/pipelines/sam2segnet/errors.py
@@ -1,29 +1,32 @@
+import torch
+
+
 class MaskNotAvailable(Exception):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("Masks are not set by user. Please set the masks using set_current_masks method.")
 
 
 class MaskShapeError(Exception):
-    def __init__(self, shape):
+    def __init__(self, shape: torch.Size) -> None:
         super().__init__(f"The masks should be a 4D tensor with shape (N, C, H, W). Got shape: {shape}")
 
 
 class ModelNotSupportedError(Exception):
-    def __init__(self, model_name, supported_models):
+    def __init__(self, model_name: str, supported_models: list[str]) -> None:
         super().__init__(
             f"Model {model_name} is not supported. Please choose one of the supported models: {supported_models}"
         )
 
 
 class FormatNotSupportedError(Exception):
-    def __init__(self, format_name, supported_formats):
+    def __init__(self, format_name, supported_formats) -> None:
         super().__init(
             f"Format {format_name} is not supported. Please choose one of the supported formats: {supported_formats}"
         )
 
 
 class ModeError(Exception):
-    def __init__(self, mode):
+    def __init__(self, mode) -> None:
         super().__init(
             f"Mode {mode} is not supported. Please choose one of the supported modes: ['train', 'val', 'test']"
         )
