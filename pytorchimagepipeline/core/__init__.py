d518f0a3054f9152c54e9e51f69d5e8836720503
Matti Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2025-11-14 14:21:26 +0100

    refactor[core]: provide core permanences and processes

--- pytorchimagepipeline/core/__init__.py
+++ pytorchimagepipeline/core/__init__.py
@@ -1,12 +1,21 @@
-# from .permanences import Device, ProgressManager, WandBLogger
-# from .processes import ResultProcess
-#
-# permanences_to_register = {
-#    "Device": Device,
-#    "ProgressManager": ProgressManager,
-#    "WandBLogger": WandBLogger,
-# }
-# processes_to_register = {
-#    "ResultProcess": ResultProcess,
-# }
-#
+from pytorchimagepipeline.abstractions import Permanence, PipelineProcess
+from pytorchimagepipeline.core.permanences import (
+    Device,
+    ProgressManager,
+    WandBManager,
+)
+
+permanences_to_register: dict[str, type[Permanence]] = {
+    "device": Device,
+    "progress_manager": ProgressManager,
+    "wandb_logger": WandBManager,
+}
+
+processes_to_register: dict[str, type[PipelineProcess]] = {
+    # "result": ResultProcess,
+}
+
+__all__ = [
+    "permanences_to_register",
+    "processes_to_register",
+]
