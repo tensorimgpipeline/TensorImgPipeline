86e1c560349935073a8835fa96d83fa9c2658708
Matti Kaupenjohann <matti.kaupenjohann@fh-dortmund.de>
2025-12-08 15:51:40 +0000

    remove[tests]: Remove mock_home_dir and setup_fake_pipeline
    This was originially implemented for e2e but fails the e2e purpose if mocking to much.
    Instead the isolated_path_manager is used exclusivly

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,5 +1,5 @@
 """Pytest configuration and fixtures for e2e tests."""
 
-from .fixtures import cli_runner, mock_home_dir, setup_fake_pipeline
+from .fixtures import cli_runner
 
-__all__ = ["cli_runner", "mock_home_dir", "setup_fake_pipeline"]
+__all__ = ["cli_runner"]
